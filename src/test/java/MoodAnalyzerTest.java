88fb668fe18a26d33bfd706e6b13b304ea539389
moin21 <mamoinuddin@gmail.com>
2022-03-15 11:07:06 +0530

    Added: Test for sad mood

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -4,18 +4,21 @@ import org.junit.Test;
 import com.moodanalyzer.MoodAnalyzer;
 
 /**
- * We have created this test method to check the sad mood.
- * If we provide the string of sad then the method should return sad.
- * We have used the assert equals to check if the method o/p is sad.
- * @author Tom
+ * Class with Mood Analyzer Tests
+ * @author Moinuddin
  *
  */
 public class MoodAnalyzerTest {
+	/**
+	 * Method to check the sad mood. Provided string with sad then method should
+	 * return sad. Used the assert equals to check if the method output is sad.
+	 * 
+	 */
 	@Test
-    public void givenMessage_IsProper_ShouldReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String actualResult = moodAnalyzer.analyseMood("I am Sad Mood");
-        Assert.assertEquals("Sad", actualResult);
-    }
+	public void givenMessage_IsProper_ShouldReturnSad() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String actualResult = moodAnalyzer.analyseMood("I am Sad Mood");
+		Assert.assertEquals("Sad", actualResult);
+	}
 
 }
