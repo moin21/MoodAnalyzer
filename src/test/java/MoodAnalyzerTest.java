292764174985fe4865ceb70ecc3fd3e037da90ae
moin21 <mamoinuddin@gmail.com>
2022-03-15 11:09:59 +0530

    Added: Test for Happy Mood

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -15,10 +15,20 @@ public class MoodAnalyzerTest {
 	 * 
 	 */
 	@Test
-	public void givenMessage_IsProper_ShouldReturnSad() {
+	public void givenMessage_IsSad_ShouldReturnSad() {
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
 		String actualResult = moodAnalyzer.analyseMood("I am Sad Mood");
 		Assert.assertEquals("Sad", actualResult);
 	}
-
+	/**
+	 * Method to check the happy mood. Provided string with happy then method should
+	 * return happy. Used the assert equals to check if the method output is happy.
+	 * 
+	 */
+	@Test
+	public void givenMessage_IsHappy_ShouldReturnHappy() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String actualResult = moodAnalyzer.analyseMood("I am in Happy Mood");
+		Assert.assertEquals("Happy", actualResult);
+	}
 }
